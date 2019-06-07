311d0e262d15d95a644e00115dccb34c5cec2cb6
Anzen Digital <alex.c.valera94@hotmail.com>
2019-06-07 18:02:43 -0500

    First components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './bootstrap.min.css';
+import Header from './components/Header';
+
+class App extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div class="container">
+                <Header title="Administrador Pacientes Veterinaria" />
+            </div>
+        );
+    }
+}
+
+export default App;
